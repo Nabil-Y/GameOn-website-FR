4c3c67b49cb3f86fe971dc3d3d62118730477cde
Nabil Yassine <nabil.yassine.dev@gmail.com>
2022-02-12 00:33:46 +0100

    Added radio button and terms/conditions check

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,14 +2,32 @@
 // DOM Elements
 /////////////////////////////////
 
+// navigation element
 const topNav = document.querySelector("#myTopnav");
+
+//mobile navigation hamburger icon
 const navBtn = document.querySelector("#navBtn");
 
-const modalBg = document.querySelector(".bground");
+// modal window
+const modalWindow = document.querySelector(".bground");
+
+// modal form
 const modalForm = document.querySelector("#modalForm");
+
+// Display modal button
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+
+//Close modal button
 const modalClose = document.querySelector(".close");
+
+// NodeList of all input fields in the form
+const formData = document.querySelectorAll(".formData");
+
+// Radio buttons array
+const radioBtns = Array.from(document.querySelectorAll(".checkbox-input[type=radio]"));
+
+// terms and conditions checkbox
+const terms = document.getElementById("checkbox1");
 
 /////////////////////////////////
 // Functions
@@ -17,17 +35,27 @@ const modalClose = document.querySelector(".close");
 
 
 // Display nav on mobile
-const editNav = () => {
-  if (topNav.className === "topnav") {
-    topNav.className += " responsive";
-  } else {
-    topNav.className = "topnav";
-  }
-}
+const toggleNav = () => topNav.classList.toggle("responsive");
+
+//Display or Hide Modal
+const toggleModal = () => modalWindow.classList.toggle("block");
 
 // form validation
 const validate = (event) => {
-  console.log("blabla");
+  
+  // Radio buttons check
+  radioBtns.every(item => item.checked === false) 
+    ? radioBtns[0].closest(".formData").setAttribute("data-error-visible", "true") 
+    : radioBtns[0].closest(".formData").setAttribute("data-error-visible", "false")
+  ;
+
+  // terms and conditions check
+  terms.checked 
+    ? terms.parentElement.setAttribute("data-error-visible", "false") 
+    : terms.parentElement.setAttribute("data-error-visible", "true")
+  ;
+
+  // Stop auto reload on submit 
   event.preventDefault();
 }
 
@@ -36,13 +64,13 @@ const validate = (event) => {
 /////////////////////////////////
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click",  () => modalBg.style.display = "block" ));
+modalBtn.forEach((btn) => btn.addEventListener("click", toggleModal));
 
 // Close event modal 
-modalClose.addEventListener("click", () => modalBg.style.display = "none");
+modalClose.addEventListener("click", toggleModal);
 
 // Topnav menu display event
-navBtn.addEventListener("click", editNav);
+navBtn.addEventListener("click", toggleNav);
 
 // Form validation submit event 
 modalForm.addEventListener("submit", validate);
